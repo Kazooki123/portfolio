146878f501991ac7266aa558d556c2e97ca45f76
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2024-10-27 18:24:59 +0800

    added sidebars (changes will be made later on)

---
+++ components/SideBar.tsx
@@ -0,0 +1,67 @@
+import { Contact, Home, Rss, HandCoins, Newspaper } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Blog",
+    url: "/blogs",
+    icon: Newspaper,
+  },
+  {
+    title: "Contact Me",
+    url: "/contact",
+    icon: Contact,
+  },
+  {
+    title: "Fund",
+    url: "/fundme",
+    icon: HandCoins,
+  },
+  {
+    title: "Live",
+    url: "/live",
+    icon: Rss,
+  },
+];
+
+export function CoolSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Portfolio</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
